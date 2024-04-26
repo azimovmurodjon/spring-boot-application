589bf123383fb5c0a95db228a03951def02e318f
azimovmurodjon <azimovm.j@gmail.com>
2024-04-25 21:16:15 -0400

    Added record GreetResponce Adding record Person List off liked Programming Languages

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 /**
  * It's syntactic sugar for combining other annotations that we'll look at in just a moment.
@@ -29,7 +31,31 @@ public class Main {
      * An abbreviated form of @RequestMapping specifically for
      * HTTP GET requests, which only takes an optional value argument, no method argument. The read in CRUD.
      */
-    public String greet(){
-        return "Hello";
+
+    public GreetResponce greet() {
+
+        GreetResponce responce = new GreetResponce(
+                "Hello",
+                List.of("Java", "Python", "JavaScript"),
+                new Person("Alex", 28, 300_00)
+        );
+        return responce;
     }
+
+    /**
+     * @RestController The @RestController annotation is a convenience syntax for @Controller
+     * and @ResponseBody together. This indicates that the class is a controller,
+     * and that all the methods in the marked class will return a JSON response.
+     * param GreetResponce
+     */
+    record Person(String nam, int age, double savings) {
+    }
+
+    record GreetResponce(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person
+    ) {
+    }
+
 }
