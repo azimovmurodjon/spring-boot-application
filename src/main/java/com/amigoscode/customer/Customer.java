5cdfacdf99fd3da9199e99ae86193cf9a4b17ca5
azimovmurodjon <azimovm.j@gmail.com>
2024-04-29 06:56:07 -0400

    Adding @Column nullable = false method to the Data

--- src/main/java/com/amigoscode/customer/Customer.java
+++ src/main/java/com/amigoscode/customer/Customer.java
@@ -21,8 +21,17 @@ public class Customer {
     )
 
     private Integer id;
+    @Column(
+          nullable = false
+    )
     private String name;
+    @Column(
+            nullable = false
+    )
     private String email;
+    @Column(
+            nullable = false
+    )
     private Integer age;
 
     public Customer() {
